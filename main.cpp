af9682b7f440ffb68c6032d1d1ef19957a172627
Senuri Rupasinghe <senuri.rupasinghe@rutgers.edu>
2025-04-21 12:21:51 -0400

    adding ahadamard and gate defs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "quantum_state.hpp"
+#include "gates.hpp"
+#include <cmath>
 
 int main() {
     QuantumState q;
@@ -8,9 +10,21 @@ int main() {
         { {0.0, 0.0}, {1.0, 0.0} },
         { {1.0, 0.0}, {0.0, 0.0} }
     };
-    q.apply_gate(X);
+
+    double inv_sqrt2 = 1.0 / std::sqrt(2.0);
+    std::vector<std::vector<std::complex<double>>> H = {
+        { {inv_sqrt2, 0.0}, {inv_sqrt2, 0.0} },
+        { {inv_sqrt2, 0.0}, {-inv_sqrt2, 0.0} }
+    };
+
+    q.apply_gate(Gates::X);
     std::cout << "\nAfter applying X gate:\n";
     q.print_state();
+
+    QuantumState q2;
+    q2.apply_gate(Gates::H);
+    std::cout << "\nAfter applying Hadamard gate:\n";
+    q2.print_state();
 
     return 0;
 }
